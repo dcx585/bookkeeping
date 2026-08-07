421963124e55557169f0696879d6058dca181bf4
Minis User <user@minis.local>
2026-08-07 19:41:29 +0800

    修复Service Worker缓存旧版导致GitHub Pages差两代的问题

--- sw.js
+++ sw.js
@@ -1,12 +1,4 @@
-const CACHE='bookkeeping-v1';
-const ASSETS=['app.html','manifest.json','icon.svg'];
-
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch',e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
-  );
-});
+const CACHE='bookkeeping-v2';
+self.addEventListener('install',e=>{e.waitUntil(self.skipWaiting())});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>clients.claim()))});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>new Response('离线',{status:503})))});
