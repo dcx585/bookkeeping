9f552c8f08868ccc08d8dc57b5297593371fe615
dcx585 <1541577924@qq.com>
2026-08-07 21:41:31 +0800

    fix PWA manifest path + cache-first sw

--- sw.js
+++ sw.js
@@ -1,4 +1,17 @@
-const CACHE='bookkeeping-v2';
-self.addEventListener('install',e=>{e.waitUntil(self.skipWaiting())});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>clients.claim()))});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>new Response('离线',{status:503})))});
+const CACHE='bookkeeping-v3';
+const URLS=['./','./app.html','./manifest.json','./icon.svg'];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>clients.claim()));
+});
+
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    if(res.ok){const rc=res.clone();caches.open(CACHE).then(c=>c.put(e.request,rc))}
+    return res;
+  })));
+});
